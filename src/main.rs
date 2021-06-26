471fe8990ece09cb8ca4396c1adf4ab15ddd96ac
Alex Rionov <misterrio535@gmail.com>
2021-06-26 22:51:16 +0300

    Init, check crypto libs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rsa::{PublicKey, RSAPrivateKey, RSAPublicKey, PaddingScheme};
+use rand::rngs::OsRng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = OsRng;
+    let bits = 2048;
+    let private_key = RSAPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+    let public_key = RSAPublicKey::from(&private_key);
+
+// Encrypt
+    let data = b"hello world";
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+    let enc_data = public_key.encrypt(&mut rng, padding, &data[..]).expect("failed to encrypt");
+    println!("{}", String::from_utf8_lossy(&*enc_data));
+
+// Decrypt
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+    let dec_data = private_key.decrypt(padding, &enc_data).expect("failed to decrypt");
+    println!("{}", String::from_utf8_lossy(&*dec_data));
 }
